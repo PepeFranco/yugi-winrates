f537f68bd78591a538e192abc8afb4ae5bf1213e
Pepe Franco <pepeg@seek.com.au>
2020-07-27 00:12:06 +1000

    fixed ordering

--- pages/api/sortRecords.js
+++ pages/api/sortRecords.js
@@ -6,13 +6,15 @@ const sortRecords = ({ recordsWithPercentages, order }) => {
   }
   if (order === "rating") {
     return recordsWithPercentages.sort((recordA, recordB) => {
-      if (recordA.totalGames === 0) return 1;
+      if (recordA.totalGames === 0 && recordB.totalGames > 0) return 1;
+      if (recordB.totalGames === 0 && recordA.totalGames > 0) return -1;
       return recordA.rating < recordB.rating ? -1 : 1;
     });
   }
   if (order === "winrate") {
     return recordsWithPercentages.sort((recordA, recordB) => {
-      if (recordA.totalGames === 0) return 1;
+      if (recordA.totalGames === 0 && recordB.totalGames > 0) return 1;
+      if (recordB.totalGames === 0 && recordA.totalGames > 0) return -1;
       return recordA.winPercentage < recordB.winPercentage ? -1 : 1;
     });
   }
