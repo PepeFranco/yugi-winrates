aa0cbab83a636bc0539ad7b60ba306f165fec6e3
Pepe Franco <pepeg@seek.com.au>
2020-07-26 18:21:19 +1000

    Migrating to dynamo

--- pages/api/records/index.js
+++ pages/api/records/index.js
@@ -1,18 +1,30 @@
-import { mongoMiddleware, apiHandler } from "../connection";
+import AWS from "aws-sdk";
 
-export default mongoMiddleware(async (req, res, connection, models) => {
-  const { method } = req;
-  apiHandler(res, method, {
-    GET: (response) => {
-      models.Record.find({}, (error, record) => {
-        if (error) {
-          connection.close();
-          response.status(500).json({ error });
-        } else {
-          response.status(200).json(record);
-          connection.close();
-        }
-      });
-    },
+export default (req, res) => {
+  AWS.config.update({
+    region: "us-east-1",
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   });
-});
+
+  const ddb = new AWS.DynamoDB({
+    apiVersion: "2012-08-10",
+  });
+
+  const params = {
+    // ExpressionAttributeValues: {},
+    TableName: "yugi-winrates",
+  };
+
+  ddb.scan(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(data));
+    }
+  });
+};
