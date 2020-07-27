1a22b9856f6bbb9339c1338316f018a75d072498
Pepe Franco <pepeg@seek.com.au>
2020-07-27 21:40:46 +1000

    Fix sizes on main page

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,28 @@ const Home = () => (
                 style={{
                   display: "flex",
                   flexDirection: "column",
-                  width: "200px",
+                  justifyContent: "space-between",
+                  width: "150px",
                   padding: "20px",
                 }}
               >
-                <img
+                <div
                   style={{
-                    height: "200px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
                   }}
-                  key={deck.code}
-                  src={`/${deck.code}.jpg`}
-                ></img>
+                >
+                  <img
+                    style={{
+                      width: "100%",
+                    }}
+                    key={deck.code}
+                    src={`/${deck.code}.jpg`}
+                  />
+                </div>
                 <h3>
                   <a href="#">{deck.name}</a>
                 </h3>
