a9f18b2f0cc752ae147607ce8baf18d624dd33f6
Pepe Franco <jose.franco@outlook.com>
2020-08-22 19:55:37 +1000

    Refactored sorting and added tests

--- pages/api/sortRecords.js
+++ pages/api/sortRecords.js
@@ -1,34 +1,46 @@
+const hasOpponent = (record) => Boolean(record.opponentDeckName);
+
+const sortRecordsAlphabetically = (records) =>
+  records.sort((recordA, recordB) =>
+    hasOpponent(recordA) && hasOpponent(recordB)
+      ? recordA.opponentDeckName.localeCompare(recordB.opponentDeckName)
+      : recordA.deckName.localeCompare(recordB.deckName)
+  );
+
+const sortRecordsByRating = (records) => {
+  return records.sort((recordA, recordB) => {
+    if (recordA.rating === recordB.rating) {
+      return recordA.totalGames < recordB.totalGames ? 1 : -1;
+    }
+    return recordA.rating < recordB.rating ? 1 : -1;
+  });
+};
+
+const sortRecordsByWinrate = (records) => {
+  return records.sort((recordA, recordB) => {
+    if (recordA.winPercentage === recordB.winPercentage)
+      return recordA.totalGames < recordB.totalGames ? 1 : -1;
+    return recordA.winPercentage < recordB.winPercentage ? 1 : -1;
+  });
+};
+
+const sortRecordsByTotalGames = (records) =>
+  records.sort((recordA, recordB) =>
+    recordA.totalGames < recordB.totalGames ? 1 : -1
+  );
+
 const sortRecords = ({ recordsWithPercentages, order }) => {
   if (order === "alphabetical") {
-    return recordsWithPercentages.sort((recordA, recordB) => {
-      if (recordA.opponentDeckName && recordB.opponentDeckName)
-        return recordA.opponentDeckName < recordB.opponentDeckName ? -1 : 1;
-      else return recordA.deckName < recordB.deckName ? -1 : 1;
-    });
+    return sortRecordsAlphabetically(recordsWithPercentages);
   }
   if (order === "rating") {
-    return recordsWithPercentages.sort((recordA, recordB) => {
-      if (recordA.totalGames === 0 && recordB.totalGames > 0) return 1;
-      if (recordB.totalGames === 0 && recordA.totalGames > 0) return -1;
-      if (recordA.rating === recordB.rating) {
-        return recordA.totalGames < recordB.totalGames ? 1 : -1;
-      }
-      return recordA.rating < recordB.rating ? 1 : -1;
-    });
+    return sortRecordsByRating(recordsWithPercentages);
   }
   if (order === "winrate") {
-    return recordsWithPercentages.sort((recordA, recordB) => {
-      if (recordA.totalGames === 0 && recordB.totalGames > 0) return 1;
-      if (recordB.totalGames === 0 && recordA.totalGames > 0) return -1;
-      if (recordA.winPercentage === recordB.winPercentage)
-        return recordA.totalGames < recordB.totalGames ? 1 : -1;
-      return recordA.winPercentage < recordB.winPercentage ? 1 : -1;
-    });
+    return sortRecordsByWinrate(recordsWithPercentages);
   }
   if (order === "totalGames") {
-    return recordsWithPercentages.sort((recordA, recordB) => {
-      return recordA.totalGames < recordB.totalGames ? 1 : -1;
-    });
+    return sortRecordsByTotalGames(recordsWithPercentages);
   }
   return recordsWithPercentages;
 };
---
+++ pages/api/sortRecords.test.js
@@ -0,0 +1,274 @@
+import sortRecords from "./sortRecords";
+const sampleRecords = [
+  {
+    deckCode: "SD3",
+    deckName: "Blaze of Destruction",
+    opponentDeckCode: "SD6",
+    opponentDeckName: "Spellcaster's Judgment",
+    wins: 0,
+    losses: 0,
+    totalGames: 0,
+    winPercentage: 0,
+    lossPercentage: 0,
+    rating: 0,
+  },
+  {
+    deckCode: "SD3",
+    deckName: "Blaze of Destruction",
+    opponentDeckCode: "SD7",
+    opponentDeckName: "Invincible Fortress",
+    wins: 0,
+    losses: 0,
+    totalGames: 0,
+    winPercentage: 0,
+    lossPercentage: 0,
+    rating: 0,
+  },
+  {
+    deckCode: "SD3",
+    deckName: "Blaze of Destruction",
+    opponentDeckCode: "SD8",
+    opponentDeckName: "Lord of the Storm",
+    wins: 0,
+    losses: 0,
+    totalGames: 0,
+    winPercentage: -1,
+    lossPercentage: 0,
+    rating: 0,
+  },
+];
+
+describe("alphabetical", () => {
+  const order = "alphabetical";
+  it("sorts based on opponent name", () => {
+    const recordsWithPercentages = [
+      {
+        opponentDeckName: "Spellcaster's Judgment",
+      },
+      {
+        opponentDeckName: "Zombie Madness",
+      },
+      {
+        opponentDeckName: "Dinosaur's Rage",
+      },
+    ];
+    const result = [
+      {
+        opponentDeckName: "Dinosaur's Rage",
+      },
+      {
+        opponentDeckName: "Spellcaster's Judgment",
+      },
+      {
+        opponentDeckName: "Zombie Madness",
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+
+  it("sorts based on deck name if no opponents deck name", () => {
+    const recordsWithPercentages = [
+      {
+        deckName: "Spellcaster's Judgment",
+      },
+      {
+        deckName: "Zombie Madness",
+      },
+      {
+        deckName: "Dinosaur's Rage",
+      },
+    ];
+    const result = [
+      {
+        deckName: "Dinosaur's Rage",
+      },
+      {
+        deckName: "Spellcaster's Judgment",
+      },
+      {
+        deckName: "Zombie Madness",
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+});
+
+describe("rating", () => {
+  const order = "rating";
+  it("sorts based on rating", () => {
+    const recordsWithPercentages = [
+      {
+        rating: 50,
+      },
+      {
+        rating: 100,
+      },
+      {
+        rating: 0,
+      },
+    ];
+    const result = [
+      {
+        rating: 100,
+      },
+      {
+        rating: 50,
+      },
+      {
+        rating: 0,
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+
+  it("tie breaks with total games", () => {
+    const recordsWithPercentages = [
+      {
+        rating: 50,
+        totalGames: 10,
+      },
+      {
+        rating: 100,
+      },
+      {
+        rating: 50,
+        totalGames: 5,
+      },
+    ];
+    const result = [
+      {
+        rating: 100,
+      },
+      {
+        rating: 50,
+        totalGames: 10,
+      },
+      {
+        rating: 50,
+        totalGames: 5,
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+});
+
+describe("winrate", () => {
+  const order = "winrate";
+  it("sorts based on winrate", () => {
+    const recordsWithPercentages = [
+      {
+        winPercentage: 32.3,
+      },
+      {
+        winPercentage: 65.6,
+      },
+      {
+        winPercentage: 49,
+      },
+    ];
+    const result = [
+      {
+        winPercentage: 65.6,
+      },
+      {
+        winPercentage: 49,
+      },
+      {
+        winPercentage: 32.3,
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+
+  it("tie breaks with total games", () => {
+    const recordsWithPercentages = [
+      {
+        winPercentage: 32.3,
+        totalGames: 9,
+      },
+      {
+        winPercentage: 65.6,
+      },
+      {
+        winPercentage: 32.3,
+        totalGames: 4,
+      },
+    ];
+    const result = [
+      {
+        winPercentage: 65.6,
+      },
+      {
+        winPercentage: 32.3,
+        totalGames: 9,
+      },
+      {
+        winPercentage: 32.3,
+        totalGames: 4,
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+
+  it("puts records with games above records with no games", () => {
+    const recordsWithPercentages = [
+      {
+        winPercentage: -1,
+        totalGames: 9,
+      },
+      {
+        winPercentage: 65.6,
+        totalGames: 2,
+      },
+      {
+        winPercentage: -1,
+        totalGames: -1,
+      },
+    ];
+    const result = [
+      {
+        winPercentage: 65.6,
+        totalGames: 2,
+      },
+      {
+        winPercentage: -1,
+        totalGames: 9,
+      },
+      {
+        winPercentage: -1,
+        totalGames: -1,
+      },
+    ];
+
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+});
+
+describe("totalGames", () => {
+  const order = "totalGames";
+  it("sorts based on totalGames", () => {
+    const recordsWithPercentages = [
+      {
+        totalGames: 10,
+      },
+      {
+        totalGames: 20,
+      },
+      {
+        totalGames: 5,
+      },
+    ];
+    const result = [
+      {
+        totalGames: 20,
+      },
+      {
+        totalGames: 10,
+      },
+      {
+        totalGames: 5,
+      },
+    ];
+    expect(sortRecords({ recordsWithPercentages, order })).toEqual(result);
+  });
+});
