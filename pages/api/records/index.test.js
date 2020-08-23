8c72d76b0ea0ebb0518ee7d28a5d12c87b36385e
Pepe Franco <jose.franco@outlook.com>
2020-08-23 19:33:48 +1000

    Added test for all records

---
+++ pages/api/records/index.test.js
@@ -0,0 +1,67 @@
+import { transformDBItemsToRecordsArray } from "./index";
+import decks, { purple, green, red, aqua } from "../../../decks";
+
+const sumTo = (n) => (n * (n + 1)) / 2;
+
+const totalRecords = sumTo(decks.length - 1);
+
+it("returns the correct number of possible records", () => {
+  expect(transformDBItemsToRecordsArray([]).length).toEqual(totalRecords);
+});
+
+it("transforms DB Items to Records Array with Percentages", () => {
+  const sampleRecords = [
+    { id: "1596351183154", winner: "SD2", loser: "SD1" },
+    { id: "1596937979871", winner: "SD1", loser: "SD2" },
+    { id: "1595773811346", winner: "SD2", loser: "SD1" },
+    { id: "1596351183155", winner: "SD3", loser: "SD1" },
+    { id: "1596937979872", winner: "SD1", loser: "SD3" },
+  ];
+  const result = transformDBItemsToRecordsArray(sampleRecords);
+  expect(result).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        deckCode: "SD2",
+        deckName: "Zombie Madness",
+        deckColor: purple,
+        opponentDeckCode: "SD1",
+        opponentDeckName: `Dragon's Roar`,
+        opponentDeckColor: green,
+        wins: 2,
+        losses: 1,
+        totalGames: 3,
+        winPercentage: 200 / 3,
+        lossPercentage: 100 / 3,
+        rating: 100 - (200 / 3 - 50),
+      }),
+      expect.objectContaining({
+        deckCode: "SD1",
+        deckName: `Dragon's Roar`,
+        deckColor: green,
+        opponentDeckCode: "SD3",
+        opponentDeckName: "Blaze of Destruction",
+        opponentDeckColor: red,
+        wins: 1,
+        losses: 1,
+        totalGames: 2,
+        winPercentage: 50,
+        lossPercentage: 50,
+        rating: 100,
+      }),
+      expect.objectContaining({
+        deckCode: "SD3",
+        deckName: `Blaze of Destruction`,
+        deckColor: red,
+        opponentDeckCode: "SD4",
+        opponentDeckName: "Fury from the Deep",
+        opponentDeckColor: aqua,
+        wins: 0,
+        losses: 0,
+        totalGames: 0,
+        winPercentage: 0,
+        lossPercentage: 0,
+        rating: 0,
+      }),
+    ])
+  );
+});
