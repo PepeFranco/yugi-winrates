45bf5ff5c2fe4f96dc1ebd69b182a353ab8677b4
PepeFranco <jose.franco@outlook.com>
2021-10-03 21:39:19 +1100

    goat

--- collectionScripts/goatRequiredCards.js
+++ collectionScripts/goatRequiredCards.js
@@ -1,32 +1,31 @@
 const _ = require("lodash");
 const fs = require("fs");
+const axios = require("axios");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const getCardInfo = async (cardId) => {
+  const id = `${cardId.trim()}`;
+  const result = await axios
+    .get(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php?id=" +
+        encodeURIComponent(id)
+    )
+    .catch(() => {});
+
+  // console.log(result.data.data[0]);
+  // console.log("==================");
+  return (result && result.data.data[0]) || null;
+};
 
 const collection = require("./collection.json");
+const goat = require("./goat.json");
 
-const cardsInStructureDeck = collection.filter(
+const cardsNotInSD = collection.filter(
   (card) =>
-    card["In Deck"].includes("Structure Deck") ||
-    card["In Deck"].includes("Egyptian")
+    !card["In Deck"].includes("Structure Deck") &&
+    !card["In Deck"].includes("Egyptian")
 );
-
-const structureDecks = _.uniq(
-  cardsInStructureDeck.map((card) => card["In Deck"])
-);
-
-const deckCardPair = cardsInStructureDeck.map((card) => ({
-  card: card["Name"],
-  deck: card["In Deck"],
-}));
-const uniqueCardsInSD = _.uniqBy(
-  deckCardPair,
-  (dcp) => `${dcp.card}-${dcp.deck}`
-);
-
-console.log("All SD cards: ", deckCardPair.length);
-console.log("Unique SD cards: ", uniqueCardsInSD.length);
-
-const uniqueCardsTimes2 = [...uniqueCardsInSD].concat([...uniqueCardsInSD]);
-const uniqueCardsTimes3 = [...uniqueCardsTimes2].concat([...uniqueCardsInSD]);
 
 const allCollectionCards = collection.map((card) => ({
   card: card["Name"],
@@ -34,136 +33,32 @@ const allCollectionCards = collection.map((card) => ({
   code: card["Code"],
 }));
 
-console.log("All cards in collection: ", allCollectionCards.length);
-console.log("Unique SD cards:    ", uniqueCardsInSD.length);
-console.log("Unique SD cards x2: ", uniqueCardsTimes2.length);
-console.log("Unique SD cards x3: ", uniqueCardsTimes3.length);
+const cardsIOwn = [];
+const cardsIMiss = [];
 
-const cardsIAlreadyOwn = [];
-
-allCollectionCards.map((cc) => {
-  const cardIndex = uniqueCardsInSD.findIndex(
-    (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  );
-
-  if (cardIndex >= 0) {
-    uniqueCardsInSD.splice(cardIndex, 1);
-
-    // console.log(`Removing ${cc.card} is in Structure deck x2 list`);
+const mainFunction = async () => {
+  for (let i = 0; i < goat.length; i++) {
+    const card = goat[i];
+    try {
+      const cardInfo = await getCardInfo(card["ID"]);
+      const cardIndex = allCollectionCards.findIndex(
+        (cc) => cc.card.toLowerCase() === cardInfo["name"].toLowerCase()
+      );
+      if (cardIndex >= 0) {
+        // console.log(
+        //   `I own ${cardInfo["name"]} (${allCollectionCards[cardIndex].code}) in ${allCollectionCards[cardIndex].location}`
+        // );
+        cardsIOwn.push(allCollectionCards[cardIndex]);
+        allCollectionCards.splice(cardIndex, 1);
+      } else {
+        // console.log(`I do NOT own ${cardInfo["name"]}`);
+        console.log(`1 ${cardInfo["name"]}`);
+        cardsIMiss.push(cardInfo["name"]);
+      }
+    } catch (e) {}
+    await sleep(100);
   }
-
-  const cardIndex2 = uniqueCardsTimes2.findIndex(
-    (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  );
-
-  if (cardIndex2 >= 0) {
-    uniqueCardsTimes2.splice(cardIndex2, 1);
-    // console.log(`Removing ${cc.card} is in Structure deck x2 list`);
-  }
-
-  const cardIndex3 = uniqueCardsTimes3.findIndex(
-    (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  );
-
-  if (cardIndex3 >= 0) {
-    uniqueCardsTimes3.splice(cardIndex3, 1);
-    if (cc.location.toLowerCase() !== "sticker") {
-      cardsIAlreadyOwn.push(cc);
-    }
-    // console.log(`Removing ${cc.card} is in Structure deck x3 list`);
-  }
-  // console.log(cc.card, cardBelongsInSD);
-});
-
-console.log("=====After removal=======");
-console.log("Unique SD cards:    ", uniqueCardsInSD.length);
-console.log("Unique SD cards x2: ", uniqueCardsTimes2.length);
-console.log("Unique SD cards x3: ", uniqueCardsTimes3.length);
-
-const cardsPerDeck2 = _.sortBy(
-  structureDecks.map((sd) => {
-    const cards = uniqueCardsTimes2.filter((uc) => uc.deck === sd);
-    return { deck: sd, cards: cards.length };
-  }),
-  (cd) => cd.cards
-);
-
-const cardsPerDeck3 = _.sortBy(
-  structureDecks.map((sd) => {
-    const cards = uniqueCardsTimes3.filter((uc) => uc.deck === sd);
-    return { deck: sd, cards: cards.length };
-  }),
-  (cd) => cd.cards
-);
-
-console.log(cardsPerDeck2);
-console.log(cardsPerDeck3);
-
-const cardsToString = (cardArray) =>
-  cardArray.reduce((prev, curr) => {
-    if (typeof prev === "string") {
-      return `${prev}\n1 ${curr.card}`;
-    }
-    return `1 ${prev.card}\n1 ${curr.card}`;
-  });
-
-const sortedUniqueCardsTimes2 = uniqueCardsTimes2.sort((a, b) => {
-  const cardsNeededInDeckA = cardsPerDeck2.find((d) => d.deck === a.deck).cards;
-  const cardsNeededInDeckB = cardsPerDeck2.find((d) => d.deck === b.deck).cards;
-  if (cardsNeededInDeckA < cardsNeededInDeckB) {
-    return -1;
-  }
-  if (cardsNeededInDeckA > cardsNeededInDeckB) {
-    return 1;
-  }
-  if (a.card < b.card) {
-    return -1;
-  }
-  if (a.card > b.card) {
-    return 1;
-  }
-  return 0;
-});
-
-fs.writeFile(
-  "cardsNeededToCompleteSDTimes2.json",
-  JSON.stringify(sortedUniqueCardsTimes2),
-  function (err) {
-    console.error(err);
-  }
-);
-
-const sortedUniqueCardsTimes3 = uniqueCardsTimes3.sort((a, b) => {
-  const cardsNeededInDeckA = cardsPerDeck3.find((d) => d.deck === a.deck).cards;
-  const cardsNeededInDeckB = cardsPerDeck3.find((d) => d.deck === b.deck).cards;
-  if (cardsNeededInDeckA < cardsNeededInDeckB) {
-    return -1;
-  }
-  if (cardsNeededInDeckA > cardsNeededInDeckB) {
-    return 1;
-  }
-  if (a.card < b.card) {
-    return -1;
-  }
-  if (a.card > b.card) {
-    return 1;
-  }
-  return 0;
-});
-
-fs.writeFile(
-  "cardsNeededToCompleteSDTimes3.json",
-  JSON.stringify(sortedUniqueCardsTimes3),
-  function (err) {
-    console.error(err);
-  }
-);
-
-console.log(cardsIAlreadyOwn.length);
-fs.writeFile(
-  "cardsIOwnForStructureDecks.json",
-  JSON.stringify(cardsIAlreadyOwn),
-  function (err) {
-    console.error(err);
-  }
-);
+  console.log(`Own: ${cardsIOwn.length}`);
+  console.log(`Miss: ${cardsIMiss.length}`);
+};
+mainFunction();
