96eece507a5f3bcab2568e575bcc3e46b581bfb8
PepeFranco <jose.franco@outlook.com>
2021-10-07 21:24:28 +1100

    Moved files around

---
+++ collectionScripts/data/downloadCollection.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const fs = require("fs");
+const csv = require("csvtojson");
+const collectionSecret = require("../../secret/collectionread.json");
+
+const mainFunction = async () => {
+  const result = await axios.get(collectionSecret.url).catch(() => {});
+  csv()
+    .fromString(result.data)
+    .then((collection) => {
+      fs.writeFile(
+        "./collectionScripts/data/collection.json",
+        JSON.stringify(collection, null, 3),
+        (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+    });
+};
+mainFunction();
---
+++ collectionScripts/formats/goatRequiredCards.js
@@ -0,0 +1,64 @@
+const _ = require("lodash");
+const fs = require("fs");
+const axios = require("axios");
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const getCardInfo = async (cardId) => {
+  const id = `${cardId.trim()}`;
+  const result = await axios
+    .get(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php?id=" +
+        encodeURIComponent(id)
+    )
+    .catch(() => {});
+
+  // console.log(result.data.data[0]);
+  // console.log("==================");
+  return (result && result.data.data[0]) || null;
+};
+
+const collection = require("../data/collection.json");
+const goat = require("./goat.json");
+
+const cardsNotInSD = collection.filter(
+  (card) =>
+    !card["In Deck"].includes("Structure Deck") &&
+    !card["In Deck"].includes("Egyptian")
+);
+
+const allCollectionCards = collection.map((card) => ({
+  card: card["Name"],
+  location: card["In Box"],
+  code: card["Code"],
+}));
+
+const cardsIOwn = [];
+const cardsIMiss = [];
+
+const mainFunction = async () => {
+  for (let i = 0; i < goat.length; i++) {
+    const card = goat[i];
+    try {
+      const cardInfo = await getCardInfo(card["ID"]);
+      const cardIndex = allCollectionCards.findIndex(
+        (cc) => cc.card.toLowerCase() === cardInfo["name"].toLowerCase()
+      );
+      if (cardIndex >= 0) {
+        // console.log(
+        //   `I own ${cardInfo["name"]} (${allCollectionCards[cardIndex].code}) in ${allCollectionCards[cardIndex].location}`
+        // );
+        cardsIOwn.push(allCollectionCards[cardIndex]);
+        allCollectionCards.splice(cardIndex, 1);
+      } else {
+        // console.log(`I do NOT own ${cardInfo["name"]}`);
+        console.log(`1 ${cardInfo["name"]}`);
+        cardsIMiss.push(cardInfo["name"]);
+      }
+    } catch (e) {}
+    await sleep(100);
+  }
+  console.log(`Own: ${cardsIOwn.length}`);
+  console.log(`Miss: ${cardsIMiss.length}`);
+};
+mainFunction();
