e37dcdbc2a00e719c88d98f7fa41393376b57d9d
PepeFranco <jose.franco@outlook.com>
2021-11-22 22:09:47 +1100

    Move

--- collectionScripts/structureDecks/moveCards.js
+++ collectionScripts/structureDecks/moveCards.js
@@ -5,7 +5,7 @@ const collection = require("../data/collection.json");
 const collectionCopy = [...collection];
 const cardsFromBP = require("./cardsIAlreadyOwnToComplete3Sets.json");
 
-const cardsToMove = cardsFromBP["Gold Sarcophagus Tin"];
+const cardsToMove = cardsFromBP["Double box"];
 
 cardsToMove.map((cc) => {
   const cardIndex = collection.findIndex(
