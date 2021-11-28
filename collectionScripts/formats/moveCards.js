d989b045e5f356efd08d1a7394e868109a707b6c
PepeFranco <jose.franco@outlook.com>
2021-11-28 20:07:14 +1100

    Moving stuff

--- collectionScripts/formats/moveCards.js
+++ collectionScripts/formats/moveCards.js
@@ -5,7 +5,7 @@ const collection = require("../data/collection.json");
 const collectionCopy = [...collection];
 const cardsFromFormat = require("./edisonOwnedCards.json");
 
-const cardsToMove = cardsFromFormat["Double box"];
+const cardsToMove = cardsFromFormat["Duel Devastator Blue"];
 
 let moved = 0;
 cardsToMove.map((cc) => {
