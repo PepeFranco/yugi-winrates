35ba40539f0f6628d44de4b67fbf7594f9b6bf78
PepeFranco <jose.franco@outlook.com>
2021-11-28 19:33:43 +1100

    Moving

--- collectionScripts/formats/moveCards.js
+++ collectionScripts/formats/moveCards.js
@@ -5,7 +5,7 @@ const collection = require("../data/collection.json");
 const collectionCopy = [...collection];
 const cardsFromFormat = require("./edisonOwnedCards.json");
 
-const cardsToMove = cardsFromFormat["Bulk (White)"];
+const cardsToMove = cardsFromFormat["Binder"];
 
 let moved = 0;
 cardsToMove.map((cc) => {
