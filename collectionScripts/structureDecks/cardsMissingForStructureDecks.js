91511b7ce8659707533d08a9c491fdeae1f08a76
PepeFranco <jose.franco@outlook.com>
2022-12-28 18:53:34 +1100

    script behaves as previous one

--- collectionScripts/structureDecks/cardsMissingForStructureDecks.js
+++ collectionScripts/structureDecks/cardsMissingForStructureDecks.js
@@ -58,319 +58,185 @@ const mainFunction = async () => {
     `📦 There are ${collection.length} cards in the collection not currently in structure decks`
   );
 
-  const structureDeckSetOfTwo = structureDecks.map((structureDeck) => {
+  const structureDeckSetOfTwoMissing = [];
+  const structureDeckSetOfThreeMissing = [];
+  structureDecks.map((structureDeck) => {
+    structureDeckSetOfTwoMissing.push({ deck: structureDeck, cards: [] });
+    structureDeckSetOfThreeMissing.push({ deck: structureDeck, cards: [] });
+  });
+
+  const getClosestMatchingBanList = (d) => {
+    for (let i = 0; i < banLists.length; i++) {
+      const thisBl = banLists[i];
+      if (i === banLists.length - 1) {
+        return thisBl;
+      }
+      const nextBl = banLists[i + 1];
+      const nextBlDate = new Date(nextBl.date);
+      if (
+        nextBlDate.getFullYear() === d.getFullYear() &&
+        nextBlDate.getMonth() === d.getMonth()
+      )
+        return nextBl;
+      if (nextBlDate > d) return thisBl;
+    }
+  };
+
+  structureDecks.map((structureDeck) => {
     const deckInCollection = structureDeckSetOfOne.find(
       (sd) => sd.deck === structureDeck
     );
+
     const uniqueCardsInDeck = _.uniq(deckInCollection.cards);
     const repeatedCards = _.filter(deckInCollection.cards, (val, i, iteratee) =>
+      _.includes(iteratee, val, i + 1)
+    );
+    const repeatedCardsTwice = _.filter(repeatedCards, (val, i, iteratee) =>
       _.includes(iteratee, val, i + 1)
     );
     const cardsNotRepeated = _.difference(
       deckInCollection.cards,
       repeatedCards
     );
+
+    const cardsNotRepeatedTwice = _.difference(
+      deckInCollection.cards,
+      repeatedCardsTwice
+    );
     console.log("-------------------------");
     console.log(
       `${deckInCollection.deck} has ${deckInCollection.cards.length} cards`
     );
+
+    const thisReleaseDate = new Date(
+      cardSets.find((set) => set["set_name"] === structureDeck)["tcg_date"]
+    );
+    const thisBanlist = getClosestMatchingBanList(thisReleaseDate);
+    // console.log(`Closest matching banlist date: ${thisBanlist.date}`);
+    const limitedCardsInThisDeck = thisBanlist.cards
+      .filter(
+        (banlistCard) =>
+          uniqueCardsInDeck.includes(banlistCard.card) &&
+          banlistCard.number === 1
+      )
+      .map((banlistCard) => banlistCard.card);
+
+    const semiLimitedCardsInThisDeck = thisBanlist.cards
+      .filter(
+        (banlistCard) =>
+          uniqueCardsInDeck.includes(banlistCard.card) &&
+          banlistCard.number === 2
+      )
+      .map((banlistCard) => banlistCard.card);
+
     console.log(`${uniqueCardsInDeck.length} unique cards`);
     if (repeatedCards.length) {
       console.log(`${cardsNotRepeated.length} cards not repeated`);
-      console.log(`${repeatedCards.length} repeated cards`);
+      console.log(`${repeatedCards.length} repeated cards once`);
       console.log(`Cards repeated: ${repeatedCards}`);
     }
+    if (repeatedCardsTwice.length) {
+      console.log(`${cardsNotRepeatedTwice.length} cards not repeated twice`);
+      console.log(`${repeatedCardsTwice.length} repeated cards twice`);
+      console.log(`Cards repeated twice: ${repeatedCardsTwice}`);
+    }
+
+    if (limitedCardsInThisDeck.length > 0) {
+      console.log(`Limited cards in deck: ${limitedCardsInThisDeck}`);
+    }
+
+    if (semiLimitedCardsInThisDeck.length > 0) {
+      console.log(`Semi limited cards in deck: ${semiLimitedCardsInThisDeck}`);
+    }
+
+    const cardsNeededToCompleteTwoSets = _.difference(
+      cardsNotRepeated,
+      limitedCardsInThisDeck
+    );
+    console.log(
+      `Cards needed to complete two sets: ${cardsNeededToCompleteTwoSets.length}`
+    );
+    structureDeckSetOfTwoMissing.find((sd) => sd.deck === structureDeck).cards =
+      _.sortBy(cardsNeededToCompleteTwoSets);
+
+    const cardsNeededToCompleteThreeSets = [
+      ..._.difference(cardsNeededToCompleteTwoSets, semiLimitedCardsInThisDeck),
+      ..._.difference(cardsNeededToCompleteTwoSets, semiLimitedCardsInThisDeck),
+    ];
+    console.log(
+      `Cards needed to complete three sets: ${cardsNeededToCompleteThreeSets.length}`
+    );
+    structureDeckSetOfThreeMissing.find(
+      (sd) => sd.deck === structureDeck
+    ).cards = _.sortBy(cardsNeededToCompleteThreeSets);
   });
 
-  // console.log("All SD cards: ", deckCardPair.length);
-  // console.log("Unique SD cards: ", uniqueCardsInSD.length);
+  const collectionForSetOfTwo = [...collection];
+  for (let i = 0; i < collectionForSetOfTwo.length; i++) {
+    const collectionCard = collectionForSetOfTwo[i];
+    for (let j = 0; j < structureDeckSetOfTwoMissing.length; j++) {
+      const { cards, deck } = structureDeckSetOfTwoMissing[j];
+      for (let k = 0; k < cards.length; k++) {
+        const card = cards[k];
+        const missingCardIndex = k;
+        if (card === collectionCard["Name"]) {
+          cards.splice(missingCardIndex, 1);
+          j = structureDeckSetOfTwoMissing.length;
+          break;
+        }
+      }
+    }
+  }
 
-  // const uniqueCardsTimes2 = [];
-  // const uniqueCardsTimes3 = [];
+  const collectionForSetOfThree = [...collection];
+  for (let i = 0; i < collectionForSetOfThree.length; i++) {
+    const collectionCard = collectionForSetOfThree[i];
+    for (let j = 0; j < structureDeckSetOfThreeMissing.length; j++) {
+      const { cards, deck } = structureDeckSetOfThreeMissing[j];
+      for (let k = 0; k < cards.length; k++) {
+        const card = cards[k];
+        const missingCardIndex = k;
+        if (card === collectionCard["Name"]) {
+          cards.splice(missingCardIndex, 1);
+          j = structureDeckSetOfThreeMissing.length;
+          break;
+        }
+      }
+    }
+  }
 
-  // const sets = await getCardSets();
-  // const sds = _.sortBy(
-  //   sets.filter((s) => s["set_name"].toLowerCase().includes("structure deck")),
-  //   (sd) => sd["tcg_date"]
-  // );
+  const setOfTwo = structureDeckSetOfTwoMissing.map((deckEntry) => ({
+    ...deckEntry,
+    cardsMissing: deckEntry.cards.length,
+  }));
 
-  // const getClosestMatchingBanList = (d) => {
-  //   for (let i = 0; i < banLists.length; i++) {
-  //     const thisBl = banLists[i];
-  //     if (i === banLists.length - 1) {
-  //       return thisBl;
-  //     }
-  //     const nextBl = banLists[i + 1];
-  //     const nextBlDate = new Date(nextBl.date);
-  //     if (
-  //       nextBlDate.getFullYear() === d.getFullYear() &&
-  //       nextBlDate.getMonth() === d.getMonth()
-  //     )
-  //       return nextBl;
-  //     if (nextBlDate > d) return thisBl;
-  //   }
-  // };
+  fs.writeFile(
+    "./collectionScripts/structureDecks/cardsFor2Sets.json",
+    JSON.stringify(
+      _.sortBy(setOfTwo, (deckEntry) => deckEntry.cardsMissing),
+      null,
+      3
+    ),
+    function (err) {
+      // console.error(err);
+    }
+  );
 
-  // const limitedCardsPerDeck = {};
-
-  // sds.map((sd) => {
-  //   const sdname = sd["set_name"];
-  //   const cardsInThisSD = uniqueCardsInSD.filter(
-  //     (c) =>
-  //       c.deck === sdname ||
-  //       (c.deck.toLowerCase().includes("legendary deck") &&
-  //         sdname === "Legendary Hero Decks")
-  //   );
-  //   if (cardsInThisSD.length === 0) {
-  //     return;
-  //   }
-  //   const thisReleaseDate = new Date(sd["tcg_date"]);
-  //   const thisBanlist = getClosestMatchingBanList(thisReleaseDate);
-  //   console.log("-----------------");
-  //   console.log(sdname);
-  //   const limitedCards = thisBanlist.cards.filter((c) => c.number === 1);
-  //   const semiLimitedCards = thisBanlist.cards.filter((c) => c.number === 2);
-  //   const limitedCardsInThisDeck = [];
-  //   const semiCardsInThisDeck = [];
-  //   cardsInThisSD.map((c) => {
-  //     uniqueCardsTimes2.push(c);
-  //     uniqueCardsTimes3.push(c);
-  //     const limitedIndex = limitedCards.findIndex(
-  //       (cc) => cc.card.toLowerCase() === c.card.toLowerCase()
-  //     );
-  //     if (limitedIndex >= 0) {
-  //       console.log(c.card, " is limited, not adding it again");
-  //       limitedCardsInThisDeck.push(c.card);
-  //       return;
-  //     }
-  //     uniqueCardsTimes2.push(c);
-  //     uniqueCardsTimes3.push(c);
-  //     const semLimitedIndex = semiLimitedCards.findIndex(
-  //       (cc) => cc.card.toLowerCase() === c.card.toLowerCase()
-  //     );
-  //     if (semLimitedIndex >= 0) {
-  //       console.log(c.card, " is semi limited, not adding it again");
-  //       semiCardsInThisDeck.push(c.card);
-  //       return;
-  //     }
-  //     uniqueCardsTimes3.push(c);
-  //   });
-  //   limitedCardsPerDeck[sdname] = {
-  //     limited: limitedCardsInThisDeck,
-  //     semiLimited: semiCardsInThisDeck,
-  //   };
-
-  //   // console.log(thisReleaseDate, thisBanlist.date);
-  // });
-
-  // const allCollectionCards = collection.map((card) => ({
-  //   card: card["Name"],
-  //   code: card["Code"],
-  //   set: card["Set"],
-  //   deck: card["In DecK"],
-  //   sleeve: card["In Sleeve"],
-  //   attribute: card["Attribute"],
-  //   type: card["Type"].toLowerCase().includes("monster")
-  //     ? "Monster"
-  //     : card["Type"],
-  // }));
-
-  // console.log("All cards in collection: ", allCollectionCards.length);
-  // console.log("Unique SD cards:    ", uniqueCardsInSD.length);
-  // console.log("Unique SD cards x2: ", uniqueCardsTimes2.length);
-  // console.log("Unique SD cards x3: ", uniqueCardsTimes3.length);
-
-  // const cardsIAlreadyOwnToComplete2Sets = [];
-  // const cardsIAlreadyOwnToComplete3Sets = [];
-
-  // allCollectionCards.map((cc) => {
-  //   const cardIndex = uniqueCardsInSD.findIndex(
-  //     (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  //   );
-
-  //   if (cardIndex >= 0) {
-  //     uniqueCardsInSD.splice(cardIndex, 1);
-  //   }
-
-  //   const cardIndex2 = uniqueCardsTimes2.findIndex(
-  //     (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  //   );
-
-  //   if (cardIndex2 >= 0) {
-  //     cardsIAlreadyOwnToComplete2Sets.push({
-  //       ...cc,
-  //       deck: uniqueCardsTimes2[cardIndex2].deck,
-  //     });
-  //     uniqueCardsTimes2.splice(cardIndex2, 1);
-  //   }
-
-  //   const cardIndex3 = uniqueCardsTimes3.findIndex(
-  //     (uc) => uc.card.toLowerCase() === cc.card.toLowerCase()
-  //   );
-
-  //   if (cardIndex3 >= 0) {
-  //     cardsIAlreadyOwnToComplete3Sets.push({
-  //       ...cc,
-  //       deck: uniqueCardsTimes3[cardIndex3].deck,
-  //     });
-  //     uniqueCardsTimes3.splice(cardIndex3, 1);
-  //   }
-  // });
-
-  // console.log("=====After removal=======");
-  // console.log("Unique SD cards:    ", uniqueCardsInSD.length);
-  // console.log("Unique SD cards x2: ", uniqueCardsTimes2.length);
-  // console.log("Unique SD cards x3: ", uniqueCardsTimes3.length);
-
-  // console.log(
-  //   "Cards I own to complete x2 sets: ",
-  //   cardsIAlreadyOwnToComplete2Sets.length
-  // );
-  // console.log(
-  //   "Cards I own to complete x3 sets: ",
-  //   cardsIAlreadyOwnToComplete3Sets.length
-  // );
-
-  // const cardsPerDeck2 = _.sortBy(
-  //   structureDecks.map((sd) => {
-  //     const cards = uniqueCardsTimes2.filter((uc) => uc.deck === sd);
-  //     return { deck: sd, cards: cards.length };
-  //   }),
-  //   (cd) => cd.cards
-  // );
-
-  // const cardsPerDeck3 = _.sortBy(
-  //   structureDecks.map((sd) => {
-  //     const cards = uniqueCardsTimes3.filter((uc) => uc.deck === sd);
-  //     return { deck: sd, cards: cards.length };
-  //   }),
-  //   (cd) => cd.cards
-  // );
-
-  // // console.log(cardsPerDeck2);
-  // // console.log(cardsPerDeck3);
-
-  // const sortPerMissingCards2 = (a, b) => {
-  //   const cardsNeededInDeckA = cardsPerDeck2.find(
-  //     (d) => d.deck === a.deck
-  //   ).cards;
-  //   const cardsNeededInDeckB = cardsPerDeck2.find(
-  //     (d) => d.deck === b.deck
-  //   ).cards;
-  //   if (cardsNeededInDeckA < cardsNeededInDeckB) {
-  //     return -1;
-  //   }
-  //   if (cardsNeededInDeckA > cardsNeededInDeckB) {
-  //     return 1;
-  //   }
-  //   if (a.card < b.card) {
-  //     return -1;
-  //   }
-  //   if (a.card > b.card) {
-  //     return 1;
-  //   }
-  //   return 0;
-  // };
-
-  // const sortPerMissingCards3 = (a, b) => {
-  //   const cardsNeededInDeckA = cardsPerDeck3.find(
-  //     (d) => d.deck === a.deck
-  //   ).cards;
-  //   const cardsNeededInDeckB = cardsPerDeck3.find(
-  //     (d) => d.deck === b.deck
-  //   ).cards;
-  //   if (cardsNeededInDeckA < cardsNeededInDeckB) {
-  //     return -1;
-  //   }
-  //   if (cardsNeededInDeckA > cardsNeededInDeckB) {
-  //     return 1;
-  //   }
-  //   if (a.card < b.card) {
-  //     return -1;
-  //   }
-  //   if (a.card > b.card) {
-  //     return 1;
-  //   }
-  //   return 0;
-  // };
-
-  // const preDecks2 = _.groupBy(
-  //   uniqueCardsTimes2.sort(sortPerMissingCards2),
-  //   (c) => c.deck
-  // );
-
-  // const sorted2 = [];
-  // for (const [key, value] of Object.entries(preDecks2)) {
-  //   sorted2.push({ deck: key, cards: value.map((c) => c.card) });
-  // }
-
-  // const sortedUniqueCardsTimes2 = _.orderBy(sorted2, (d) => d.cards.length);
-
-  // fs.writeFile(
-  //   "./collectionScripts/structureDecks/cardsNeededToComplete2Sets.json",
-  //   JSON.stringify(sortedUniqueCardsTimes2, null, 3),
-  //   function (err) {
-  //     console.error(err);
-  //   }
-  // );
-
-  // const preDecks3 = _.groupBy(
-  //   uniqueCardsTimes3.sort(sortPerMissingCards3),
-  //   (c) => c.deck
-  // );
-
-  // const sorted3 = [];
-  // for (const [key, value] of Object.entries(preDecks3)) {
-  //   sorted3.push({ deck: key, cards: value.map((c) => c.card) });
-  // }
-
-  // const sortedUniqueCardsTimes3 = _.orderBy(sorted3, (d) => d.cards.length);
-
-  // fs.writeFile(
-  //   "./collectionScripts/structureDecks/cardsNeededToComplete3Sets.json",
-  //   JSON.stringify(sortedUniqueCardsTimes3, null, 3),
-  //   function (err) {
-  //     console.error(err);
-  //   }
-  // );
-
-  // const owned2 = _.groupBy(cardsIAlreadyOwnToComplete2Sets, (c) => c.location);
-  // for (const [key, value] of Object.entries(owned2)) {
-  //   owned2[key] = _.sortBy(value, (c) => `${c.attribute}-${c.type}-${c.card}`);
-  // }
-
-  // fs.writeFile(
-  //   "./collectionScripts/structureDecks/cardsIAlreadyOwnToComplete2Sets.json",
-  //   JSON.stringify(owned2, null, 3),
-  //   function (err) {
-  //     console.error(err);
-  //   }
-  // );
-
-  // // console.log("Cards to move per location ================");
-  // const owned3 = _.groupBy(cardsIAlreadyOwnToComplete3Sets, (c) => c.location);
-  // // Object.keys(owned3).map((l) => {
-  // //   console.log(l, owned3[l].length);
-  // // });
-  // for (const [key, value] of Object.entries(owned3)) {
-  //   owned3[key] = _.sortBy(value, (c) => `${c.attribute}-${c.type}-${c.card}`);
-  // }
-
-  // fs.writeFile(
-  //   "./collectionScripts/structureDecks/cardsIAlreadyOwnToComplete3Sets.json",
-  //   JSON.stringify(owned3, null, 3),
-  //   function (err) {
-  //     console.error(err);
-  //   }
-  // );
-
-  // fs.writeFile(
-  //   "./collectionScripts/structureDecks/limitedCardsPerDeck.json",
-  //   JSON.stringify(limitedCardsPerDeck, null, 3),
-  //   function (err) {
-  //     console.error(err);
-  //   }
-  // );
+  const setOfThree = structureDeckSetOfThreeMissing.map((deckEntry) => ({
+    ...deckEntry,
+    cardsMissing: deckEntry.cards.length,
+  }));
+  fs.writeFile(
+    "./collectionScripts/structureDecks/cardsFor3Sets.json",
+    JSON.stringify(
+      _.sortBy(setOfThree, (deckEntry) => deckEntry.cardsMissing),
+      null,
+      3
+    ),
+    function (err) {
+      // console.error(err);
+    }
+  );
 };
 
 mainFunction();
