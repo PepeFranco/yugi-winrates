145281bf88aa5ab74d8009e42b7cfff32bb0d9ee
PepeFranco <jose.franco@outlook.com>
2023-08-29 20:41:34 +1000

    decks with records test

--- pages/api/decks/index.test.js
+++ pages/api/decks/index.test.js
@@ -135,146 +135,99 @@ it("returns decks with no records if no items in db", async () => {
     });
 });
 
-// it("transforms db items into record array", async () => {
-//   //arrange
-//   const sampleRecords = [
-//     { id: "1", winner: "SD2", loser: "SD1" },
-//     { id: "2", winner: "SD1", loser: "SD2" },
-//     { id: "3", winner: "SD3", loser: "SD1" },
-//     { id: "4", winner: "SD2", loser: "SD3" },
-//     { id: "5", winner: "SD1", loser: "SD2" },
-//     { id: "5", winner: "SD2", loser: "SD1" },
-//     { id: "5", winner: "SD5", loser: "SD4" },
-//     { id: "5", winner: "SD6", loser: "SD4" },
-//   ];
-//   docClient.scan.mockImplementation((params, callback) => {
-//     callback(null, { Items: sampleRecords });
-//   });
-//   const app = express();
-//   app.get("/", getRecords);
+it("transforms db items into record array", async () => {
+  //arrange
+  const sampleRecords = [
+    { id: "1", winner: "SD2", loser: "SD1" },
+    { id: "2", winner: "SD1", loser: "SD2" },
+    { id: "3", winner: "SD3", loser: "SD1" },
+    { id: "4", winner: "SD2", loser: "SD3" },
+    { id: "5", winner: "SD1", loser: "SD2" },
+    { id: "5", winner: "SD2", loser: "SD1" },
+    { id: "5", winner: "SD5", loser: "SD4" },
+    { id: "5", winner: "SD6", loser: "SD4" },
+  ];
+  docClient.scan.mockImplementation((params, callback) => {
+    callback(null, { Items: sampleRecords });
+  });
+  const app = express();
+  app.get("/", getDecks);
 
-//   //act
-//   await supertest(app)
-//     .get("/")
-//     .expect(200)
-//     .expect("Content-Type", /json/)
-//     .then((res) => {
-//       expect(res.body).toEqual([
-//         {
-//           deckCode: "SD2",
-//           deckColor: "#B57EDC",
-//           deckName: "Zombie Madness",
-//           lossPercentage: 50,
-//           losses: 2,
-//           opponentDeckCode: "SD1",
-//           opponentDeckColor: "#50C878",
-//           opponentDeckName: "Dragon's Roar",
-//           rating: 100,
-//           totalGames: 4,
-//           winPercentage: 50,
-//           wins: 2,
-//           type: "structure",
-//         },
-//         {
-//           deckCode: "SD6",
-//           deckColor: "#FADFAD",
-//           deckName: "Spellcaster's Judgment",
-//           lossPercentage: 0,
-//           losses: 0,
-//           opponentDeckCode: "SD4",
-//           opponentDeckColor: "#81D8D0",
-//           opponentDeckName: "Fury from the Deep",
-//           rating: 50,
-//           totalGames: 1,
-//           winPercentage: 100,
-//           wins: 1,
-//           type: "structure",
-//         },
-//         {
-//           deckCode: "SD5",
-//           deckColor: "#FF9966",
-//           deckName: "Warrior's Triumph",
-//           lossPercentage: 0,
-//           losses: 0,
-//           opponentDeckCode: "SD4",
-//           opponentDeckColor: "#81D8D0",
-//           opponentDeckName: "Fury from the Deep",
-//           rating: 50,
-//           totalGames: 1,
-//           winPercentage: 100,
-//           wins: 1,
-//           type: "structure",
-//         },
-//         {
-//           deckCode: "SD3",
-//           deckColor: "#CD5C5C",
-//           deckName: "Blaze of Destruction",
-//           lossPercentage: 0,
-//           losses: 0,
-//           opponentDeckCode: "SD1",
-//           opponentDeckColor: "#50C878",
-//           opponentDeckName: "Dragon's Roar",
-//           rating: 50,
-//           totalGames: 1,
-//           winPercentage: 100,
-//           wins: 1,
-//           type: "structure",
-//         },
-//         {
-//           deckCode: "SD2",
-//           deckColor: "#B57EDC",
-//           deckName: "Zombie Madness",
-//           lossPercentage: 0,
-//           losses: 0,
-//           opponentDeckCode: "SD3",
-//           opponentDeckColor: "#CD5C5C",
-//           opponentDeckName: "Blaze of Destruction",
-//           rating: 50,
-//           totalGames: 1,
-//           winPercentage: 100,
-//           wins: 1,
-//           type: "structure",
-//         },
-//       ]);
-//     });
-// });
-
-// it("puts winners on the left", async () => {
-//   //arrange
-//   const sampleRecords = [
-//     { id: "1", winner: "SD2", loser: "SD1" },
-//     { id: "2", winner: "SD1", loser: "SD2" },
-//     { id: "3", winner: "SD1", loser: "SD2" },
-//     { id: "3", winner: "SD1", loser: "SD2" },
-//   ];
-//   docClient.scan.mockImplementation((params, callback) => {
-//     callback(null, { Items: sampleRecords });
-//   });
-//   const app = express();
-//   app.get("/", getRecords);
-
-//   //act
-//   await supertest(app)
-//     .get("/")
-//     .expect(200)
-//     .expect("Content-Type", /json/)
-//     .then((res) => {
-//       expect(res.body).toEqual([
-//         {
-//           deckCode: "SD1",
-//           deckName: "Dragon's Roar",
-//           deckColor: "#50C878",
-//           opponentDeckCode: "SD2",
-//           opponentDeckName: "Zombie Madness",
-//           opponentDeckColor: "#B57EDC",
-//           wins: 3,
-//           losses: 1,
-//           totalGames: 4,
-//           winPercentage: 75,
-//           lossPercentage: 25,
-//           rating: 75,
-//           type: "structure",
-//         },
-//       ]);
-//     });
-// });
+  //act
+  await supertest(app)
+    .get("/")
+    .expect(200)
+    .expect("Content-Type", /json/)
+    .then((res) => {
+      expect(res.body).toEqual(
+        expect.arrayContaining([
+          {
+            deckCode: "SD1",
+            deckName: "Dragon's Roar",
+            deckColor: "#50C878",
+            wins: 2,
+            losses: 3,
+            totalGames: 5,
+            winPercentage: 40,
+            lossPercentage: 60,
+            type: "structure",
+          },
+          {
+            deckCode: "SD2",
+            deckName: "Zombie Madness",
+            deckColor: "#B57EDC",
+            wins: 3,
+            losses: 2,
+            totalGames: 5,
+            winPercentage: 60,
+            lossPercentage: 40,
+            type: "structure",
+          },
+          {
+            deckCode: "SD3",
+            deckName: "Blaze of Destruction",
+            deckColor: "#CD5C5C",
+            wins: 1,
+            losses: 1,
+            totalGames: 2,
+            winPercentage: 50,
+            lossPercentage: 50,
+            type: "structure",
+          },
+          {
+            deckCode: "SD4",
+            deckName: "Fury from the Deep",
+            deckColor: "#81D8D0",
+            wins: 0,
+            losses: 2,
+            totalGames: 2,
+            winPercentage: 0,
+            lossPercentage: 100,
+            type: "structure",
+          },
+          {
+            deckCode: "SD5",
+            deckName: "Warrior's Triumph",
+            deckColor: "#FF9966",
+            wins: 1,
+            losses: 0,
+            totalGames: 1,
+            winPercentage: 100,
+            lossPercentage: 0,
+            type: "structure",
+          },
+          {
+            deckCode: "SD6",
+            deckName: "Spellcaster's Judgment",
+            deckColor: "#FADFAD",
+            wins: 1,
+            losses: 0,
+            totalGames: 1,
+            winPercentage: 100,
+            lossPercentage: 0,
+            type: "structure",
+          },
+        ])
+      );
+    });
+});
